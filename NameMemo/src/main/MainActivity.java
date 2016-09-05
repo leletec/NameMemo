5f6bdfa6213bb236a42fd768c60d7e503c4fba7c
Leander Dreier <leander3@outlook.de>
2016-09-05 12:55:14 +0200

    Design stuff

--- NameMemo/src/main/MainActivity.java
+++ NameMemo/src/main/MainActivity.java
@@ -464,7 +464,7 @@ public class MainActivity extends Activity implements OnClickListener {
 					@Override
 					public void onClick(DialogInterface dialog, int which) {
 						EditText name = (EditText) ((AlertDialog) dialog)
-								.findViewById(R.id.ETname);
+								.findViewById(R.id.etName);
 						if (f == null)
 							pictureDb.add(camera.getUri().getPath(), name.getText().toString(), Picture.Camera);
 						else {
